d010ac5c6e1173fd3d48139a26d7b623e42d3cb3
Luthfi Dwi Syah Putra <luthputra02@gmail.com>
2023-06-05 22:01:17 +0700

    tes

--- math.py
+++ math.py
@@ -4,7 +4,7 @@
 #         int factorial = calculateFactorial(number);
 #         System.out.println("Factorial of " + number + " is: " + factorial);
 #     }
-    
+
 #     public static int calculateFactorial(int n) {
 #         if (n == 0) {
 #             return 1;
